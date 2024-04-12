36d68b4c043ca6c0962478d281c5258d6eb45f5c
jaydendonham <jbd0017@mix.wvu.edu>
2024-04-11 21:38:16 -0400

    added flight stuff

---
+++ SP24FlightSolutionDonham/SP24MVCDonham/Controllers/FlightController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SP24ClassLibraryDonham;
+using SP24MVCDonham.Models;
+using SP24MVCDonham.ViewModels;
+
+namespace SP24MVCDonham.Controllers
+{
+    public class FlightController : Controller
+    {
+
+        private IFlightRepo iFlightRepo;
+
+        public FlightController(IFlightRepo flightRepo)
+        {
+            this.iFlightRepo = flightRepo;
+        }
+
+        //METHOD SIGNATURE
+        //3 - Name, Return Type, Number & Order of Parameters
+        //2 or more with same name = Method Overloading
+        [HttpGet]
+        public IActionResult SearchFlights()
+        {
+            SearchFlightsViewModel viewModel = new SearchFlightsViewModel();
+            return View(viewModel);
+        }
+        //SEARCH BUTTON
+        [HttpPost]
+        public IActionResult SearchFlights(SearchFlightsViewModel viewModel)//Search Inputs
+        {
+            //Get all flights
+            List<Flight> flights = this.iFlightRepo.ListAllFlights();
+
+            //if user searches by departure airport, filter result
+            if(viewModel.DepartureAirportID != null)
+            {
+                flights = flights.Where(f => f.DepartureAirportID == viewModel.DepartureAirportID).ToList();
+            }
+
+            //if user searches by arrival airport, filter result
+            if (viewModel.ArrivalAirportID != null)
+            {
+                flights = flights.Where(f => f.ArrivalAirportID == viewModel.ArrivalAirportID).ToList();
+            }
+
+            //return result to user
+            viewModel.SearchResult = flights;
+            return View(viewModel);
+        }
+    }
+}
---
+++ SP24FlightSolutionDonham/SP24MVCDonham/ViewModels/SearchFlightsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SP24ClassLibraryDonham;
+
+namespace SP24MVCDonham.ViewModels
+{
+    public class SearchFlightsViewModel
+    {
+        //Search Criteria
+        //Inputs
+        public int DepartureAirportID { get; set; }
+        public int ArrivalAirportID { get; set; }
+
+        //Result
+        public List<Flight> SearchResult {  get; set; }
+    }
+}
